b6cfe915b4c3b74c4371638d94979c02b9459a9d
unknown <you@example.com>
2025-01-31 14:33:58 +0500

    shome changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <>
       <h2> <p>Salom saalat</p> </h2>
+
+      <div className="salomsaaalat">
+        <h2 className="eh">salom</h2>
+      </div>
     </>
   )
 }
